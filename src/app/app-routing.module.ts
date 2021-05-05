039d5fd562c8415914faece1e0046fcc8b710f9e
github-Krunal <githubkrunal@gmail.com>
2021-05-06 02:23:51 +0530

    login page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login', loadChildren: () => import('./component/account/login/login.module').then((login) => login.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/account/login/login.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularMaterialModule } from 'src/app/shared/angular-material/angular-material.module';
+
+const login: Routes = [{
+  path: '',
+  component:LoginComponent
+}]
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(login),
+    AngularMaterialModule
+  ]
+})
+export class LoginModule { }
