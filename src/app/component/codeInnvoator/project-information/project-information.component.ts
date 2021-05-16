2e7a138df8f1e2d6c8dbfafa42ecaa62dcf0b4fd
github-Krunal <githubkrunal@gmail.com>
2021-05-17 00:56:52 +0530

    project details radion button event

--- src/app/component/codeInnvoator/project-information/project-information.component.ts
+++ src/app/component/codeInnvoator/project-information/project-information.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
 
 @Component({
   selector: 'app-project-information',
@@ -7,10 +8,27 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation:ViewEncapsulation.None
 })
 export class ProjectInformationComponent implements OnInit {
-  public disabled: boolean = true;
+  public xyz: string = "";
+  public abc: any[] = [{
+    name: "section1",
+    images:'../../../../assets/images/tech (2).jpg'
+  },
+  {
+    name: "section2",
+    images:'../../../../assets/images/tech (3).jpg'
+  },
+  {
+    name: "section3",
+    images:'../../../../assets/images/social1 (4).jpg'
+  }]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.xyz = this.abc[0].name;
   }
-
+  radioChange(event: MatRadioChange) {
+    this.xyz = event.value.name;
+  }
 }
+  
