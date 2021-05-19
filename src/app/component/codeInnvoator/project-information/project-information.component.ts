4910769512a2f53a1a1b968f14659c222a8ecd07
github-Krunal <githubkrunal@gmail.com>
2021-05-20 00:36:50 +0530

    project details animation

--- src/app/component/codeInnvoator/project-information/project-information.component.ts
+++ src/app/component/codeInnvoator/project-information/project-information.component.ts
@@ -2,7 +2,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatRadioChange } from '@angular/material/radio';
 import { RightSlideAnimation } from 'src/app/shared/animation/right-slide-animation';
-
 @Component({
   selector: 'app-project-information',
   templateUrl: './project-information.component.html',
@@ -10,28 +9,20 @@ import { RightSlideAnimation } from 'src/app/shared/animation/right-slide-animat
   animations: [RightSlideAnimation],
   encapsulation:ViewEncapsulation.None
 })
-export class ProjectInformationComponent implements OnInit {
-  public xyz: string = "";
-  public abc: any[] = [{
-    name: "section1",
-    images:'../../../../assets/images/tech (2).jpg'
-  },
-  {
-    name: "section2",
-    images:'../../../../assets/images/tech (3).jpg'
-  },
-  {
-    name: "section3",
-    images:'../../../../assets/images/social1 (4).jpg'
-  }]
 
+export class ProjectInformationComponent implements OnInit {
+  public sectionName: string = "";
+  public projectDetails: any[] = [{
+    name: "section1",
+    // images:'../../../../assets/images/tech (2).jpg'
+  }]
   constructor() { }
 
   ngOnInit(): void {
-    this.xyz = this.abc[0].name;
+    this.sectionName = this.projectDetails[0].name;
   }
   radioChange(event: MatRadioChange) {
-    this.xyz = event.value.name;
+    this.sectionName = event.value.name;
   }
 }
   
--- src/app/shared/animation/right-slide-animation.ts
+++ src/app/shared/animation/right-slide-animation.ts
@@ -1,17 +1,37 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, sequence, state, style, transition, trigger } from '@angular/animations';
 
-export  const RightSlideAnimation =
-	 trigger('slide', [
-      transition('void=>*', [
-        style({
-         transform: 'translateX(100%)'
-        }),
-        animate(1000, style({ transform: 'translateX(0%)'}))
-	  ]),
-		 transition('*=>void', [
-        style({
-         transform: 'translateX(0%)'
-        }),
-        animate(500,style({ transform: 'translateX(-100%)'}))
-      ])
-    ])
+export const RightSlideAnimation =
+// slide right to left
+    //    trigger(
+    //         'slide', [
+    //             transition(':enter', [
+    //                 style({ transform: 'translateX(100%)' }),
+    //                 animate('1s', style({ transform: 'translateX(0)' }))
+    //             ]),
+    //             transition(':leave', [
+    //                 style({ transform: 'translateX(0)', opacity: 1 }),
+    //                 animate('1.5s', style({ transform: 'translateX(-150%)' }))
+    //             ])
+    //         ]
+    //   );
+    
+    // slide up to bottom
+    // trigger('slide', [
+    //     transition(':enter', [
+    //         style({ opacity: '0', height: '0' }),
+    //         animate('1s ease-in', style({ opacity: '1', height: '*' }))
+    //     ]),
+    //     transition(':leave', [
+    //         animate('0.5s ease-out', style({ opacity: '0', height: '0' }))
+    //     ])
+    // ]);
+    // slider transition
+    trigger('slide', [
+        transition(':enter', [
+            style({ opacity: '0'}),
+            animate('1.5s ease-in', style({ opacity: '1' }))
+        ]),
+        transition(':leave', [
+            animate('0.7s ease-out', style({ opacity: '0'}))
+        ])
+    ]);
