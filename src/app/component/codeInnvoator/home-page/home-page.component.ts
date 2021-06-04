29da16d12a3ecac194c5caf66104344ec50b932c
github-Krunal <githubkrunal@gmail.com>
2021-06-05 02:13:38 +0530

    card design with comming soon messages

--- src/app/component/codeInnvoator/home-page/home-page.component.ts
+++ src/app/component/codeInnvoator/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AppIcon } from 'src/app/shared/modal/sample-modal';
 
 @Component({
   selector: 'app-home-page',
@@ -7,10 +8,55 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomePageComponent implements OnInit {
+  public abc: number[] = [];
+  public appIcon: AppIcon[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    // this.abc = Array.from({ length: 8 }, (x, i) => i);
+    this.appIcon = [
+      {
+        Id: 1021,
+        ImageUrl: '../../../../assets/images/pg (1).jpg',
+        Name: 'Task Management',
+        Time: '2 Days To Go',
+      }, {
+        Id: 1022,
+        ImageUrl: '../../../../assets/images/pg (2).jpg',
+        Name: 'Project Management',
+        Time: '1 Days To Go',
+      },
+      {
+        Id: 1023,
+        ImageUrl: '../../../../assets/images/pg (3).jpg',
+        Name: 'Calculator',
+        Time: '6 Days To Go',
+      },
+      {
+        Id: 1024,
+        ImageUrl: '../../../../assets/images/image-coming-soon.jpg',
+        Name: 'Angular',
+        Time: '1 Year To Go',
+      },
+      {
+        Id: undefined,
+        ImageUrl: '',
+        Name: 'Project',
+        Time: '',
+      },
+      {
+        CommingSoon: true
+      },
+      {
+        CommingSoon: true
+      },
+      {
+        CommingSoon: true
+      },
+      {
+        CommingSoon: true
+      }]
   }
 
 }
---
+++ src/app/shared/modal/sample-modal.ts
@@ -0,0 +1,7 @@
+export class AppIcon {
+	public Id?: number;
+	public ImageUrl?: string;
+	public Name?: string;
+	public Time?: string;
+	public CommingSoon?: boolean;
+}
