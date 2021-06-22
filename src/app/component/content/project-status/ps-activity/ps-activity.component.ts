8c7e6bec5f3b654062e0d5a16c3f3fc961e91ce7
github-Krunal <githubkrunal@gmail.com>
2021-06-23 01:05:12 +0530

    activity status complete

--- src/app/component/content/project-status/ps-activity/ps-activity.component.ts
+++ src/app/component/content/project-status/ps-activity/ps-activity.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./ps-activity.component.scss'],
 })
 export class PsActivityComponent implements OnInit {
+  public abc: number[] = [1, 2, 3, 3]
 
   constructor() { }
 
